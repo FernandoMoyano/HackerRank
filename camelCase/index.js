a6a8b1338748a2e981db04b03c8d9ce9bbf524a7
FernandoMoyano <fernandomoyano21@gmail.com>
2024-07-27 21:31:30 -0300

    solucion 2 para la operación de división

--- camelCase/index.js
+++ camelCase/index.js
@@ -1,18 +1,50 @@
-//startsWith()
-//includes()
-//toLowercase()
-//repleace()
+//------------------SOLUCION 1------------------------
+// startsWith - includes - toLowercase - repleace
+//-----------------------------------------------------
 
-function processData(input) {
-  //Enter your code here
+/* function processData(input) {
   if (input.startsWith("S")) {
     let newString = input.slice(4, input.length);
     newString = newString.replace(/([a-z])([A-Z])/g, "$1 $2").toLowerCase();
+
     return newString;
   }
 }
+ */
 
-console.log(processData("S;M:vasoDePlastico"));
+//-----------------SOLUCION 2-----------------------
+//split - map - charAt - slice - toLowerCase - toUpperCase
+//--------------------------------------------------
+
+function processData(input) {
+  let [operation, rest] = input.split(";"); //<-- S M:vasoDePlastico()
+  let [type, words] = rest.split(":"); //<-- M vasoDePlastico()
+  let result = "";
+
+  if (operation.startsWith("S")) {
+    let splitWords = "";
+
+    for (let i = 0; i <= words.length; i++) {
+      const char = words[i];
+
+      if (char === "(" || char === ")") {
+        continue; //<-- ignorar parentesis en metodos
+      }
+
+      if (char >= "A" && char <= "Z" && i > 0) {
+        splitWords += "" + char.toLowerCase();
+      } else {
+        splitWords += char;
+      }
+    }
+    result = splitWords;
+  } else if (rest.startsWith("C")) {
+  }
+}
+
+processData("S;M:vasoDePlastico()");
+
+/* console.log(processData("S;M:vasoDePlastico"));
 
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
@@ -24,3 +56,4 @@ process.stdin.on("data", function (input) {
 process.stdin.on("end", function () {
   processData(_input);
 });
+ */
